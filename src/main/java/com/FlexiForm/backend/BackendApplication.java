d92fdf83af2466495ef12b441af9e91cb9bd5523
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-05-01 16:47:54 +0530

    created application prod properties

--- src/main/java/com/FlexiForm/backend/BackendApplication.java
+++ src/main/java/com/FlexiForm/backend/BackendApplication.java
@@ -9,15 +9,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 
-
-		Dotenv dotenv = Dotenv.load();
-
-		// Set system properties for Spring Boot to access
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
