295978b263e3cd9a9a3ddf38f23c1915ed9a7838
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-05-02 15:01:34 +0530

    Updated properties in prod and dev

--- src/main/java/com/FlexiForm/backend/services/JwtService.java
+++ src/main/java/com/FlexiForm/backend/services/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+
 import javax.swing.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
